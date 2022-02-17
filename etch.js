46d9e5797c4fa93091fbeb4680f8ceca8300ee7f
Paul <paul.probst@gmail.com>
2022-02-17 13:32:52 -0500

    Added change color of cell on mouseover

--- etch.js
+++ etch.js
@@ -1,15 +1,25 @@
-// create function to generate grid squares
+
 const etchGrid = document.getElementById("etchGrid");
 
+//generate grid of divs, assign class and id to each
 function makeRows(rows, cols) {
     etchGrid.style.setProperty('--grid-rows', rows);
     etchGrid.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);  //remove
-        etchGrid.appendChild(cell).className = "grid-item";
-        etchGrid.appendChild(cell).id = "cell" + c;
+        etchGrid.appendChild(cell).className = "gridItem";
+        // etchGrid.appendChild(cell).id = "cell"; //+ c to number
     };
 };
 
 makeRows(16, 16);
+
+
+//add hover effect coloring grid cells
+const target = etchGrid.querySelectorAll('.gridItem');
+
+target.forEach(gridItem => {
+    gridItem.addEventListener('mouseover', function activeCell(event) {
+        gridItem.classList.add("active");
+    });
+});
